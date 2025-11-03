364d7b853162af017692a783341ea596fc61242b
Paulina <paulina.zwolenik@gmail.com>
2025-11-03 20:54:44 +0100

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write your code here
+const populationElements = document.querySelectorAll('span.population');
+
+const populationNumbers = Array.from(populationElements).map((element) => {
+  const rawString = element.textContent;
+  const cleanedString = rawString.replace(/,/g, '');
+
+  return +cleanedString;
+});
+
+const totalPopulation = populationNumbers.reduce((sum, currentPopulation) => {
+  return sum + currentPopulation;
+}, 0);
+
+const averagePopulation = totalPopulation / populationNumbers.length;
+
+const averageElement = document.querySelector('span.average-population');
+const totalElement = document.querySelector('span.total-population');
+
+totalElement.textContent = totalPopulation.toLocaleString('en-US', {
+  maximumFractionDigits: 0,
+});
+
+averageElement.textContent = averagePopulation.toLocaleString('en-US', {
+  minimumFractionDigits: 2,
+  maximumFractionDigits: 2,
+});
